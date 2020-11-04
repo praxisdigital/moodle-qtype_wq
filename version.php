9579878d200d7481d208275de489bd0d57d1e159
Jacob Christiansen <jch@praxis.dk>
2020-11-04 14:34:45 +0100

    Version bump

--- version.php
+++ version.php
@@ -16,7 +16,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2020000400;
+$plugin->version = 2020000401;
 $plugin->requires = 2011120500; // Moodle 2.2.
 $plugin->release = '3.78.2 (Build: Niels Brock med unlimited question connections edition)';
 $plugin->maturity = MATURITY_STABLE;
